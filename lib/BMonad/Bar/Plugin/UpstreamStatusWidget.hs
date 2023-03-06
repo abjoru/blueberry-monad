296d35a02693012a6f1897b83e10917bbfbd002e
Andreas Bjoru <andreas.bjoru@gmail.com>
2023-03-06 15:50:12 -0500

    better indication of ahead/behind

--- lib/BMonad/Bar/Plugin/UpstreamStatusWidget.hs
+++ lib/BMonad/Bar/Plugin/UpstreamStatusWidget.hs
@@ -1,15 +1,15 @@
 module BMonad.Bar.Plugin.UpstreamStatusWidget (UpstreamSettings(..)) where
 
-import BMonad.Config.Types (Color)
 import           BMonad.Bar.Icons
+import           BMonad.Config.Types (Color)
 
-import           Data.Maybe       (fromMaybe)
-import           System.Directory (XdgDirectory (XdgData), getXdgDirectory)
-import           System.Exit      (ExitCode)
-import           System.Process   (readProcess, spawnProcess, waitForProcess)
-import           Text.Read        (readMaybe)
+import           Data.Maybe          (fromMaybe)
+import           System.Directory    (XdgDirectory (XdgData), getXdgDirectory)
+import           System.Exit         (ExitCode)
+import           System.Process      (readProcess, spawnProcess, waitForProcess)
+import           Text.Read           (readMaybe)
 
-import           Xmobar           (Exec (alias, rate, run))
+import           Xmobar              (Exec (alias, rate, run))
 
 -- |Upstream widget settings.
 -- @param1 refresh rate
@@ -21,9 +21,9 @@ data UpstreamSettings = UpstreamSettings
   } deriving (Read, Show)
 
 instance Exec UpstreamSettings where
-  alias _                         = "upstream"
-  rate (UpstreamSettings r _ _ _) = r
-  run                             = runWidget
+  alias _ = "upstream"
+  rate    = upstreamRate
+  run     = runWidget
 
 runWidget :: UpstreamSettings -> IO String
 runWidget cfg = do
@@ -33,29 +33,25 @@ runWidget cfg = do
 
   let ma = readMaybe ahead :: Maybe Int
       mb = readMaybe behind :: Maybe Int
-
-  return $ formatOutput cfg (fromMaybe 0 ma) (fromMaybe 0 mb)
+   in return $ formatOutput cfg (fromMaybe 0 ma) (fromMaybe 0 mb)
 
 formatOutput :: UpstreamSettings -> Int -> Int -> String
-formatOutput (UpstreamSettings _ ac bc sc) a b 
-  | a > 0 = icoGit' ac (" " ++ show a)
-  | b > 0 = icoGit' bc (" " ++ show b)
+formatOutput (UpstreamSettings _ ac bc sc) a b
+  | a > 0 = icoGit' ac (" +" ++ show a)
+  | b > 0 = icoGit' bc (" -" ++ show b)
   | otherwise = icoGit sc
 
-readGit :: [String] -> IO String
-readGit args = do
+mkCmd :: [String] -> IO String
+mkCmd args = do
   path <- getXdgDirectory XdgData "blueberry-monad"
 
   let pushd = "pushd " ++ path ++ " &>/dev/null"
       popd  = "popd &>/dev/null"
-      cmd   = pushd ++ " ; git " ++ unwords args ++ " ; " ++ popd
-   in readProcess "bash" ["-c", cmd] []
+   in return $ pushd ++ " ; git " ++ unwords args ++ " ; " ++ popd
+
+readGit :: [String] -> IO String
+readGit args = mkCmd args >>= (\x -> readProcess "bash" ["-c", x] [])
 
 spawnGit :: [String] -> IO ExitCode
-spawnGit args = do
-  path <- getXdgDirectory XdgData "blueberry-monad"
-
-  let pushd = "pushd " ++ path ++ " &>/dev/null"
-      popd  = "popd &>/dev/null"
-      cmd   = pushd ++ " ; git " ++ unwords args ++ " ; " ++ popd
-   in waitForProcess =<< spawnProcess "bash" ["-c", cmd]
+spawnGit args = mkCmd args >>= spawnCmd
+  where spawnCmd c = waitForProcess =<< spawnProcess "bash" ["-c", c]
